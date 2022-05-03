359421109efd4459b9583d5c890762d4c99c3147
rodiemertomi <rodiemertominez@gmail.com>
2022-05-03 12:56:27 +0800

    finishes styling for profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import TomiSVG from '../Components/TomiSVG'
+import Footer from '../Components/Footer'
+import Button from '../Components/Button'
+import { Link } from 'react-router-dom'
+import '../logo.css'
 
 const Profile = () => {
   return (
-    <div className='bg-lightblue h-screen'>
-      <div className='flex flex-col gap-20 top-20 items-center justify-between text-3xl text-shadow-xl'>
-        <div>Hi, I'm</div>
-        <TomiSVG />
-        <div>A Web Developer</div>
+    <div className='shadow-md'>
+      <div className='bg-lightblue h-screen'>
+        <div className='pt-52 flex flex-col md:flex-row md:justify-around items-center gap-32 text-3xl text-shadow-xl font-Lora text-black'>
+          <div id='t-div' className='order-1'>
+            <span title='T;'>T;</span>
+          </div>
+          <div className='flex flex-col items-left gap-32 text-3xl text-shadow-xl font-Lora text-black'>
+            <div>Hi, I'm</div>
+            <TomiSVG />
+            <div>A Web Developer.</div>
+            <Button>
+              <Link to='/aboutme'>Learn More</Link>
+            </Button>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   )
 }
