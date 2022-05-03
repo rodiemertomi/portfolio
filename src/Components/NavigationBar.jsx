0060171f1e286de70f7a2128509638fd35dab3fa
rodiemertomi <rodiemertominez@gmail.com>
2022-05-03 21:42:11 +0800

    makes text bold, adds shadow

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -10,7 +10,7 @@ const NavigationBar = () => {
   ]
   let [open, setOpen] = useState(false)
   return (
-    <div className='shadow-md w-full sticky top-0 left-0 z-[1]'>
+    <div className='shadow-md w-full sticky top-0 left-0 z-[1] text-shadow-md font-bold'>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
         <div className='font-bold text-2xl md:text-3xl cursor-pointer flex items-center font-Lora text-black'>
           <div className='hover:text-lightblue transition-all duration-200'>
@@ -24,7 +24,7 @@ const NavigationBar = () => {
           <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
         </div>
         <ul
-          className={`md:flex md:items-center md:justify-between md:gap-6 gap-16 font-Lora text-2xl md:text-3xl md:py-8 pb-12 absolute lg:static bg-white md:z-auto z-[-1] left-0 w-full md:w-full md:pl-0 pl-9 md:pr-4 transition-all duration-500 ease-in-out ${
+          className={`md:flex md:items-center lg:justify-end md:justify-between lg:gap-10 md:gap-6 gap-16 font-Lora text-2xl md:text-3xl lg:py-0 md:py-8 pb-12 absolute lg:static bg-white md:z-auto z-[-1] left-0 w-full md:w-full md:pl-0 pl-9 lg:pr-0 md:pr-4 transition-all duration-500 ease-in-out ${
             open ? 'top-20' : 'top-[-300px]'
           }`}
         >
