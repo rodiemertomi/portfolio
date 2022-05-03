9d8d98deea26ef7f6e62504cbacf001476f9f49e
rodiemertomi <rodiemertominez@gmail.com>
2022-05-03 21:41:40 +0800

    adds shadow and bolds letter

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = props => {
   return (
-    <button className='border border-black bg-lightblue rounded-3xl font-Lora text-black transition-all duration-500 hover:bg-darkblue hover:text-white px-6 py-2'>
+    <button className='font-bold text-shadow-md border border-black bg-lightblue rounded-3xl font-Lora text-black transition-all duration-200 hover:bg-darkblue hover:text-white px-6 py-2'>
       {props.children}
     </button>
   )
