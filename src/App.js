152cbc88acc664a44d742fc47305e8a5c34fcb6d
rodiemertomi <rodiemertominez@gmail.com>
2022-05-03 18:46:26 +0800

    changes aboutme to aboutme2 for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavigationBar from './Components/NavigationBar'
 import Profile from './Pages/Profile'
-import AboutMe from './Pages/AboutMe'
 import Projects from './Pages/Projects'
+import AboutMe2 from './Pages/AboutMe2'
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route exact path='/' element={<Profile />} />
-        <Route exact path='/aboutme' element={<AboutMe />} />
+        <Route exact path='/aboutme' element={<AboutMe2 />} />
         <Route exact path='/Projects' element={<Projects />} />
       </Routes>
     </BrowserRouter>
