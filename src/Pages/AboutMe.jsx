02489450d19bda58afa05f174a55b5f7cb8a3732
rodiemertomi <rodiemertominez@gmail.com>
2022-06-02 23:58:42 +0800

    adds project preview carousel

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -2,41 +2,42 @@ import React, { useEffect } from 'react'
 import Footer from '../Components/Footer'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+import Carousel from 'react-elastic-carousel'
+import Button from '../Components/Button'
+import { Link } from 'react-router-dom'
 
 const AboutMe = () => {
   useEffect(() => {
     Aos.init()
   }, [])
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 768, itemsToShow: 2 },
+    { width: 1280, itemsToShow: 3 },
+  ]
+
   return (
     <>
-      <div className='shadow-md bg-lightblue'>
-        <div className='h-screen'>
-          <div className='flex flex-col justify-center items-center h-4/5 w-auto gap-20'>
-            <div className='flex justify-center items-between'>
-              <img
-                src={require('../assets/images/latest.jpg')}
-                alt='latestPicture.jpg'
-                className='rounded-full lg:w-[25%] w-[50%] shadow-xl'
-              />
-            </div>
-            <div className='text-lg lg:text-3xl font-Lora text-center text-shadow-xl text-black'>
-              Hi! I'm Rodiemer Tominez, an undergrad of Marine Transportation, <br></br>in pursuit
-              of my passion for Information Technology
-            </div>
+      <div className='bg-lightblue'>
+        <div className='flex flex-col justify-center items-center h-screen w-auto gap-20'>
+          <div className='flex justify-center items-between'>
+            <img
+              src={require('../assets/images/latest.jpg')}
+              alt='latestPicture.jpg'
+              className='rounded-full lg:w-[25%] w-[50%] shadow-xl'
+            />
+          </div>
+          <div className='text-lg lg:text-3xl font-Lora text-center text-shadow-xl text-black'>
+            Hi! I'm Rodiemer Tominez, an undergrad of Marine Transportation, <br></br>in pursuit of
+            my passion for Information Technology
           </div>
         </div>
         <div>
-          <div className='flex justify-center bg-transparent'>
-            <span
-              data-aos='fade-up'
-              data-aos-offset='250'
-              className='font-Lora text-3xl text-shadow-md text-black'
-            >
-              My Skills
-            </span>
+          <div className='flex justify-center sticky top-[68px] z-10 left-0' data-aos='fade-down'>
+            <span className='font-Lora text-3xl text-shadow-md text-black'>My Skills</span>
           </div>
-          <section className='lg:flex lg:justify-around'>
+          <section className='lg:flex lg:justify-around' data-aos='fade-up' data-aos-offset='500'>
             <div
               data-aos='fade-up'
               data-aos-offset='500'
@@ -66,7 +67,7 @@ const AboutMe = () => {
               </span>
             </div>
           </section>
-          <section className='lg:flex lg:justify-around'>
+          <section className='lg:flex lg:justify-around' data-aos='fade-up' data-aos-offset='500'>
             <div
               data-aos='fade-right'
               data-aos-offset='500'
@@ -110,7 +111,11 @@ const AboutMe = () => {
               </span>
             </div>
           </section>
-          <section className='lg:flex lg:justify-around pb-20'>
+          <section
+            className='lg:flex lg:justify-around pb-20'
+            data-aos='fade-up'
+            data-aos-offset='500'
+          >
             <div
               data-aos='fade-right'
               data-aos-offset='550'
@@ -141,8 +146,76 @@ const AboutMe = () => {
             </div>
           </section>
         </div>
-        <Footer />
+        <div className='h-screen pt-20 flex flex-col gap-20'>
+          <div
+            className='flex justify-center sticky top-[68px] z-10 left-0'
+            data-aos='fade-down'
+            data-aos-offset='500'
+          >
+            <span className='font-Lora text-3xl text-shadow-md text-black'>
+              My Projects Preview
+            </span>
+          </div>
+          <div
+            className='sticky top-[20%] translate-y-[-50%] px-10'
+            data-aos='fade-down'
+            data-aos-offset='500'
+          >
+            <Carousel breakPoints={breakPoints}>
+              <div className='flex flex-col justify-center items-center md:px-5'>
+                <img src={require('../assets/gifs/calculator.gif')} alt='' />
+                <h1>Calculator</h1>
+                <a
+                  href='https://rodiemertomi.github.io/Calculator/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <Button>Try it</Button>
+                </a>
+              </div>
+              <div className='flex flex-col justify-center items-center md:px-5'>
+                <img src={require('../assets/gifs/tictactoe.gif')} alt='' />
+                <h1>TicTacToe</h1>
+                <a
+                  href='https://rodiemertomi.github.io/TicTacToe/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <Button>Try it</Button>
+                </a>
+              </div>
+              <div className='flex flex-col justify-center items-center md:px-5'>
+                <img src={require('../assets/gifs/momentum.gif')} alt='' />
+                <h1>Momentum App</h1>
+                <a href='https://rodiemertomi.github.io/Momentum/' target='_blank' rel='noreferrer'>
+                  <Button>Try it</Button>
+                </a>
+              </div>
+              <div className='flex flex-col justify-center items-center md:px-5'>
+                <img src={require('../assets/gifs/landingpage.gif')} alt='' />
+                <h1>Product Landing Page</h1>
+                <a
+                  href='https://rodiemertomi.github.io/ProductLandingPage/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <Button>Try it</Button>
+                </a>
+              </div>
+            </Carousel>
+          </div>
+        </div>
+        <div
+          className='flex justify-center items-center lg:pb-32 lg:pt-10 pb-5 pt-5'
+          data-aos='fade-up'
+          data-aos-offset='300'
+        >
+          <Button>
+            <Link to='/projects'>See More</Link>
+          </Button>
+        </div>
       </div>
+      <Footer />
     </>
   )
 }
