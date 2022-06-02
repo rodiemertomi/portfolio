68a5a3648f54bf1b1a656e27e33e2dd6df7041af
rodiemertomi <rodiemertominez@gmail.com>
2022-06-02 13:46:53 +0800

    fixes some styling issues

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -7,13 +7,13 @@ import '../logo.css'
 
 const Profile = () => {
   return (
-    <div className='shadow-md'>
-      <div className='bg-lightblue h-screen'>
-        <div className='pt-20 md:pt-52 flex flex-col md:flex-row md:justify-around items-center gap-20 text-2xl md:text-3xl text-shadow-xl font-Lora text-black'>
+    <div className='shadow-md bg-lightblue'>
+      <div className='h-screen'>
+        <div className='xl:pt-52 pt-20 md:pt-20 flex flex-col md:flex-row md:justify-around items-center gap-20 text-2xl md:text-3xl text-shadow-xl font-Lora text-black'>
           <div id='t-div' className='md:order-1'>
             <span title='T;'>T;</span>
           </div>
-          <div className='flex flex-col items-left gap-20 text-2xl text-shadow-xl font-Lora text-black'>
+          <div className='flex flex-col items-left gap-20 text-2xl lg:text-3xl text-shadow-xl font-Lora text-black'>
             <div>Hi, I'm</div>
             <TomiSVG />
             <div>A Web Developer.</div>
